6b3e1fb40fb27ab028d48ee754cabfae16807d58
Temitope Akande <topsiology11@gmail.com>
2023-05-08 08:28:14 +0100

    first commit

---
+++ hard-hat-backend/constants/index.js
@@ -0,0 +1,5 @@
+
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x3aAb1447EC4f3F85606D11Ef9aff03F379ff986f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
